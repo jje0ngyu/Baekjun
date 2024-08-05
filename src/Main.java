9998c49d0a07be905ca513acdf8afb32066fb509
jje0ngyu <naniaerang@naver.com>
2024-08-05 23:40:34 +0900

    8/5

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import step.STEP01;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
---
+++ src/step/STEP01.java
@@ -0,0 +1,7 @@
+package step;
+
+public class STEP01 {
+    public static void s1000() {
+        System.out.println("Hello World!!!!");
+    }
+}
