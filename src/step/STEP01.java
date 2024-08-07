bda20de11e4d7bbc53d8382fc52d66b59e35b248
jje0ngyu <naniaerang@naver.com>
2024-08-07 23:09:11 +0900

    [ FEAT ] NumberFormatException 발생 사례 : 2^31 -1 보다 클 경우

--- src/step/STEP01.java
+++ src/step/STEP01.java
@@ -1,6 +1,7 @@
 package step;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class STEP01 {
     public void s1000() {
@@ -9,5 +10,25 @@ public class STEP01 {
         a = sc.nextInt();
         b = sc.nextInt();
         System.out.println(a + b);
+    }
+
+    // 조건 1 : 1 ≤ A, B, C ≤ 1012
+    // int 사용시 NumberFormatException 발생
+    // -> Integer.parseInt를 사용할 경우 2^31 -1보다 크면 Integer의 범위를 벗어나기 때문에 발생
+    public void no11382() {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        Long answer = 0L;
+        while(st.hasMoreTokens()) {
+            String token = st.nextToken();
+            answer += Long.parseLong(token);
+        }
+
+        System.out.println(answer);
+
     }
 }
