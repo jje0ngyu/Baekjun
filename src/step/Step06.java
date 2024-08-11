4dad274eb4ba43dcc681ddab8e12b2969d31cd09
jje0ngyu <naniaerang@naver.com>
2024-08-12 07:38:43 +0900

    8/12 StringBuilder

---
+++ src/step/Step06.java
@@ -0,0 +1,25 @@
+package step;
+
+public class Step06 {
+
+    /*
+                     ,r'"7
+        r`-_   ,'  ,/
+         \. ". L_r'
+           `~\/
+              |
+              |
+    */
+    public static void drowSprout(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |");
+
+        System.out.println(sb);
+    }
+}
