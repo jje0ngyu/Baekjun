e0eabde83d0b9a7d9e8b76d972e2a5ce7b100a1f
jje0ngyu <naniaerang@naver.com>
2024-08-12 07:04:45 +0900

    8/12

--- src/step/Step02.java
+++ src/step/Step02.java
@@ -1,5 +1,8 @@
 package step;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Step02 {
@@ -22,6 +25,19 @@ public class Step02 {
             System.out.println("<");
         } else if (a == b) {
             System.out.println("==");
+        }
+    }
+
+    // No.2753
+    public static void checkLeapYear(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        if ((n % 4 == 0 && n % 100 != 0)
+                || n % 400 == 0 ) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
         }
     }
 }
