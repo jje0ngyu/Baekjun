35494a10f23a392563c2aca0e967fb49acd59727
jeongYu <yjjeong@4-lab.com>
2024-08-13 11:07:00 +0900

    8/13 BufferedWriter

--- src/step/Step03.java
+++ src/step/Step03.java
@@ -1,15 +1,17 @@
 package step;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Step03 {
 
     // No.2739
-    public static void multiplicationTable(String[] args) {
+    public void multiplicationTable(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -22,7 +24,7 @@ public class Step03 {
     }
 
     // No.10950
-    public static void loofPlus(String[] args) throws IOException {
+    public void loofPlus(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T =  Integer.parseInt(br.readLine());
@@ -41,7 +43,7 @@ public class Step03 {
     }
 
     // No.25314
-    public static void repeatLong(String[] args) throws IOException {
+    public void repeatLong(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
@@ -49,5 +51,27 @@ public class Step03 {
         sb.append("int");
 
         System.out.println(sb);
+    }
+
+    // No.15552
+    public void no15552() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T, result;
+        StringTokenizer st;
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            result = 0;
+            st = new StringTokenizer(br.readLine());
+            while(st.hasMoreTokens()){
+                result += Integer.parseInt(st.nextToken());
+            }
+            bw.write(result + "\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
